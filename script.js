d67282155e7b7cf09ea4d569c72657991622c385
ZardoxGit <sounouk@hotmail.fr>
2021-05-20 14:39:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('buttonValidation').addEventListener('click',
+function(){
+    
+    var guessWord = document.getElementById('word').value.toUpperCase();
+    console.log(guessWord);
+})
